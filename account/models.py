071912b948da30b0dd77bf4de307585a7a565c6c
nadersayed22 <nadersayedrabea22@gmail.com>
2021-04-15 21:35:41 +0200

    add serializer for patient &doctor & clinic

--- account/models.py
+++ account/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 
-
 Choice_Reg = (
     ('Doctor', 'Doctor'),
     ('patient', 'patient'),
--- doctorapp/models.py
+++ doctorapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+gender = (
+    ('male', 'male'),
+    ('female', 'female')
+)
+
+
+class Patient(models.Model):
+    patient_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    gender = models.CharField(max_length=25, choices=gender)
+
+
+class Doctor(models.Model):
+    doctor_name = models.CharField(max_length=255)
+    doctor_email = models.EmailField(max_length=255, unique=True)
+
+
+class Clinic(models.Model):
+    clinic_name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    date_serve = models.DateField()
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(auto_now_add=True)
+    patient_serv = models.ForeignKey('patient', on_delete=models.CASCADE)
+    doctor_serv = models.ForeignKey('Doctor', on_delete=models.CASCADE)
